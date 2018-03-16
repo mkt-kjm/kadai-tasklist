8ac4d6918bf3adfd4fda0a7ef8127c11cdc1e25f
mkt.kjm <travelfactoryg@gmail.com>
2018-03-16 13:13:01 +0000

    modify two files

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,6 @@ class Task < ApplicationRecord
   
   #新規カラム追加 -status presenceは指定された属性が空でないことを確認する
   #空白を許さず、10文字以下を許容する
-  validates :status,presence: true, length:{maximum:10}
+  validates :content, presence: true, length: { maximum: 255 }#気づかなかった点
+  validates :status, presence: true, length: { maximum: 10 }
 end
--- db/migrate/20180314161745_add_status_to_tasks.rb
+++ db/migrate/20180314161745_add_status_to_tasks.rb
@@ -1,5 +1,8 @@
 class AddStatusToTasks < ActiveRecord::Migration[5.1]
   def change
-    add_column :content, :status, :string
+    #add_column :content, :status, :stringと書いており、エラー。
+    #add_column :xxx,のxxxは既に存在するテーブルでないとダメ。
+    #そもそもこのマイグレーションファイルは、カラム追加のためのもの
+    add_column :tasks, :status, :string
   end
 end
